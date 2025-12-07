370484ad42566001f26ae917c88c7147cbdb70a6
Srb <jakub.nowak404@gmail.com>
2025-12-07 14:46:42 +0100

    Initial commit

---
+++ src/main/java/me/Srb/nbblcoreNeo/nbblcoreNeo.java
@@ -0,0 +1,10 @@
+package me.Srb.nbblcoreNeo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class nbblcoreNeo extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+}
