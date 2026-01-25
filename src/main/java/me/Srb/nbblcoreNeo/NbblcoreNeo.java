67eee300222c5b12910ed4fbfa9deb118029f5ba
srb <jakub.nowak404@gmail.com>
2026-01-26 00:05:56 +0100

    refactor: rename main class and clean up plugin.yml

--- src/main/java/me/Srb/nbblcoreNeo/NbblcoreNeo.java
+++ src/main/java/me/Srb/nbblcoreNeo/NbblcoreNeo.java
@@ -4,10 +4,11 @@ import lombok.Getter;
 import me.Srb.nbblcoreNeo.command.challenge.ChallengeCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class nbblcoreNeo extends JavaPlugin {
+public final class NbblcoreNeo extends JavaPlugin {
 
     @Getter
-    private static nbblcoreNeo plugin;
+    private static NbblcoreNeo plugin;
+
     @Override
     public void onEnable() {
         plugin = this;
--- src/main/java/me/Srb/nbblcoreNeo/storage/FileManager.java
+++ src/main/java/me/Srb/nbblcoreNeo/storage/FileManager.java
@@ -1,6 +1,6 @@
 package me.Srb.nbblcoreNeo.storage;
 
-import me.Srb.nbblcoreNeo.nbblcoreNeo;
+import me.Srb.nbblcoreNeo.NbblcoreNeo;
 
 import java.io.File;
 import java.util.logging.Logger;
@@ -11,8 +11,8 @@ public abstract class FileManager {
     protected String path;
 
     public FileManager(String fileName) {
-        this.LOGGER = nbblcoreNeo.getPlugin().getLogger();
-        this.path = nbblcoreNeo.getPlugin().getDataFolder().getAbsolutePath() + "/" + fileName;
+        this.LOGGER = NbblcoreNeo.getPlugin().getLogger();
+        this.path = NbblcoreNeo.getPlugin().getDataFolder().getAbsolutePath() + "/" + fileName;
         this.file = new File(path);
         if (!file.getParentFile().exists()) file.getParentFile().mkdirs();
     }
